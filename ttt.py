7267c6a97ba3fdfd9357b1f604520e14f586f61a
P37uh <p3tuhd@gmail.com>
2022-11-18 21:10:58 +0300

    Fix bug and add new features 1. Fixed a bug, where the result of the game is "Draw", if the player wins on the last turn 2. The console is now automatically cleared during the game and after some commands 3. Restart command is non-silently now 4. The Play command now uses the restart command, if the last game has already been finished

--- ttt.py
+++ ttt.py
@@ -1,49 +1,49 @@
 import numpy as np
 from time import sleep
 from sys import exit as sys_exit
+from os import system
 
 class OccupationError(Exception): ...
 class CommandError(Exception): ...
+class RestartError(Exception): ...
 
 class Game():
-    
+
     '''
     Console 3x3 tic-tac-toe game
     For detailed help type help
     '''
-    
+
     def __init__(self):
         self.field = np.zeros((3, 3), dtype=str)
-        
+
     def __repr__(self):
         return '\n'.join(str(x) for x in self.field)
-    
-    def _check_win(self) -> bool:
-        if all(((self.field == 'O') | (self.field == 'X')).flatten()):
-                print('Draw!')
-                sleep(2)
-                return True
+
+    def _check_win(self) -> str:
         for symbol in ('O', 'X'):
             for _ in range(3):
                 if (all((self.field == symbol)[_]) or
                     all(np.rot90((self.field == symbol))[_]) or
                     all(np.diag((self.field == symbol))) or
                     all(np.diag(np.rot90((self.field == symbol))))):
-                    print(f'{symbol} won!')
-                    sleep(2)
-                    return True
-        return False
-        
+                    return f'{symbol} won!'
+        if all(((self.field == 'O') | (self.field == 'X')).flatten()):
+                return 'Draw'
+        return ''
+
     def _cmd_input(self):
         try:
             match input().lower():
                 case 'play':
+                    system('cls')
                     return self.play_game()
                 case 'restart':
                     return self.restart_game()
                 case 'check':
                     return self.check_field()
                 case 'help':
+                    system('cls')
                     return self.get_help()
                 case 'exit':
                     return self.exit()
@@ -53,12 +53,19 @@ class Game():
             print('Unknown command')
             return self._cmd_input()
         except KeyboardInterrupt:
+            system('cls')
             return self.open_menu()
-            
+
     def play_game(self):
+        if self._check_win():
+            self.field = np.zeros((3, 3), dtype=str)
         turn = 1
+        print(self)
         while 1:
-            if self._check_win():
+            if result := self._check_win():
+                print(result)
+                sleep(2)
+                system('cls')
                 return self.open_menu()
             try:
                 x, y = int(input('x = ')), int(input('y = '))
@@ -66,6 +73,7 @@ class Game():
                     raise OccupationError
                 self.field[x-1][y-1] = 'O' if turn else 'X'
                 turn = not turn
+                system('cls')
                 print(self)
             except IndexError:
                 print('X and Y must be between [1, 3]')
@@ -74,16 +82,24 @@ class Game():
             except OccupationError:
                 print('This cell is occupied')
             except KeyboardInterrupt:
+                system('cls')
                 return self.open_menu()
-                
+
     def restart_game(self):
-        self.field = np.zeros((3, 3), dtype=str)
-        return self._cmd_input()
-    
+        try:
+            if len(np.unique(self.field)) == 1:
+                raise RestartError
+            self.field = np.zeros((3, 3), dtype=str)
+            print('Succesfully restarted')
+        except RestartError:
+            print('Already restarted')
+        finally:
+            return self._cmd_input()
+
     def check_field(self):
         print(self)
         return self._cmd_input()
-    
+
     def get_help(self):
         print('''
     When game starts, field 3x3 will appear.
@@ -94,10 +110,10 @@ class Game():
     Note: all commands are not case sensitive.
               ''')
         return self._cmd_input()
-    
+
     def exit(self):
         sys_exit()
-        
+
     def open_menu(self):
         print('''
     Type:
@@ -106,10 +122,10 @@ class Game():
     3. 'Check' to check the last game field
     4. 'Help' for detailed help
     5. 'Exit' to exit
-    You can use the Ctrl + C combination to return to the menu at any moment. (Not finished game will be saved)
+    You can use the Ctrl + C combination to return to the menu at any moment. (Unfinished game will be saved)
               ''')
         return self._cmd_input()
-    
+
 if __name__ == "__main__":
     game = Game()
     game.open_menu()
